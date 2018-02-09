b4374b16f00c91fba5688a2029aaf8ad27cc323a
TaylorHawkes <th71852@gmail.com>
2018-02-08 18:56:52 -0600

    3d2 c

---
+++ 3d2/main.c
@@ -0,0 +1,376 @@
+#include "v4l2_driver.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <linux/videodev2.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <unistd.h>
+
+#define SAVE_EVERY_FRAME 0
+#define DEPTH_MAP 1
+#define USE_MJPEG 0
+
+pthread_t thread_stream;
+SDL_Window *sdlScreen;
+SDL_Renderer *sdlRenderer;
+SDL_Texture *sdlTexture;
+SDL_Rect sdlRect;
+
+/* miscellanous */
+int thread_exit_sig = 0;
+
+struct streamHandler {
+  int fd;
+  int fd2;
+  void (*framehandler)(void *pframe, int length,void *pframe2,int length2);
+};
+
+void print_help() {
+  printf("Usage: simple_cam <width> <height> <device>\n");
+  printf("Example: simple_cam 640 480 /dev/video0\n");
+}
+
+static void frame_handler(void *pframe, int length,void *pframe2, int length2) {
+
+v4lconvert_yuyv_to_rgb24((unsigned char *) pframe, data_buffer1);
+v4lconvert_yuyv_to_rgb24((unsigned char *) pframe2, data_buffer2);
+
+compare_images();
+
+
+#if !USE_MJPEG
+
+  //SDL_UpdateTexture(sdlTexture, &sdlRect,pframe , IMAGE_WIDTH * 2);
+  SDL_UpdateTexture(sdlTexture, &sdlRect, final_image, IMAGE_WIDTH * 3);
+  //  SDL_UpdateYUVTexture
+  SDL_RenderClear(sdlRenderer);
+  SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect);
+  SDL_RenderPresent(sdlRenderer);
+#endif
+
+#if USE_MJPEG
+	SDL_RWops *buf_stream = SDL_RWFromMem(pframe, (int)length);
+	SDL_Surface *frame = IMG_Load_RW(buf_stream, 0);
+	SDL_Texture *tx = SDL_CreateTextureFromSurface(sdlRenderer, frame);
+
+	SDL_RenderClear(sdlRenderer);
+	SDL_RenderCopy(sdlRenderer, tx, NULL, NULL);
+	SDL_RenderPresent(sdlRenderer);
+
+	SDL_DestroyTexture(tx);
+	SDL_FreeSurface(frame);
+	SDL_RWclose(buf_stream);
+#endif
+
+#if DEPTH_MAP 
+
+#endif
+
+#if SAVE_EVERY_FRAME
+
+  static yuv_index = 0;
+//if(yuv_index < 1){
+  char yuvifle[100];
+  sprintf(yuvifle, "yuv-%d.yuv", yuv_index);
+  FILE *fp = fopen(yuvifle, "wb");
+  fwrite(pframe, length, 1, fp);
+  fclose(fp);
+  yuv_index++;
+//}
+
+#endif
+}
+
+static void *v4l2_streaming(void *arg) {
+
+
+  // SDL2 begins
+  memset(&sdlRect, 0, sizeof(sdlRect));
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER)) {
+    printf("Could not initialize SDL - %s\n", SDL_GetError());
+    return NULL;
+  }
+
+  sdlScreen = SDL_CreateWindow("Simple YUV Window", SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED, IMAGE_WIDTH,
+                               IMAGE_HEIGHT, SDL_WINDOW_SHOWN);
+
+  if (!sdlScreen) {
+    fprintf(stderr, "SDL: could not create window - exiting:%s\n",
+            SDL_GetError());
+    return NULL;
+  }
+
+  sdlRenderer = SDL_CreateRenderer(
+      sdlScreen, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (sdlRenderer == NULL) {
+    fprintf(stderr, "SDL_CreateRenderer Error\n");
+    return NULL;
+  }
+    
+    //SDL_PIXELFORMAT_YUY2
+    //SDL_PIXELFORMAT_RGB24
+
+#if !USE_MJPEG
+  sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, IMAGE_WIDTH, IMAGE_HEIGHT);
+  sdlRect.w = IMAGE_WIDTH;
+  sdlRect.h = IMAGE_HEIGHT;
+#endif
+
+#if USE_MJPEG
+if (!IMG_Init(IMG_INIT_JPG)) {
+    fprintf(stderr, "Unable  ti init image Error\n");
+    return -1;
+}
+#endif
+
+  int fd = ((struct streamHandler *)(arg))->fd;
+  int fd2 = ((struct streamHandler *)(arg))->fd2;
+  void (*handler)(void *pframe, int length,void *prame2, int length2) =
+      ((struct streamHandler *)(arg))->framehandler;
+
+  fd_set fds;
+  fd_set fds2;
+  struct v4l2_buffer buf;
+  struct v4l2_buffer buf2;
+  while (!thread_exit_sig) {
+    int ret;
+    int ret2;
+
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+
+    FD_ZERO(&fds2);
+    FD_SET(fd2, &fds2);
+
+    struct timeval tv = {.tv_sec = 5, .tv_usec = 0};
+    ret = select(fd + 1, &fds, NULL, NULL, &tv);
+    ret2 = select(fd2 + 1, &fds2, NULL, NULL, &tv);
+
+    if (-1 == ret) {
+      fprintf(stderr, "select error\n");
+      return NULL;
+    } else if (0 == ret) {
+      fprintf(stderr, "timeout waiting for frame\n");
+      continue;
+    }
+    if (-1 == ret2) {
+      fprintf(stderr, "select error\n");
+      return NULL;
+    } else if (0 == ret2){
+      fprintf(stderr, "timeout waiting for 2nd frame\n");
+      continue;
+    }
+
+    //camera 1
+    if (FD_ISSET(fd, &fds)) {
+      memset(&buf, 0, sizeof(buf));
+      buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+      buf.memory = V4L2_MEMORY_MMAP;
+      if (-1 == ioctl(fd, VIDIOC_DQBUF, &buf)) {
+        fprintf(stderr, "VIDIOC_DQBUF failure\n");
+        return NULL;
+      }
+
+///  if (handler)
+///    (*handler)(v4l2_ubuffers[buf.index].start,
+///               v4l2_ubuffers[buf.index].length);
+
+      buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+      buf.memory = V4L2_MEMORY_MMAP;
+      if (-1 == ioctl(fd, VIDIOC_QBUF, &buf)) {
+        fprintf(stderr, "VIDIOC_QBUF failure\n");
+        return NULL;
+      }
+    }
+
+    //camera 2
+    if (FD_ISSET(fd2, &fds2)) {
+      memset(&buf2, 0, sizeof(buf2));
+      buf2.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+      buf2.memory = V4L2_MEMORY_MMAP;
+      if (-1 == ioctl(fd2, VIDIOC_DQBUF, &buf2)) {
+        fprintf(stderr, "VIDIOC_DQBUF 2 failure\n");
+        return NULL;
+      }
+
+    if (handler)
+      (*handler)(v4l2_ubuffers[buf.index].start,
+                 v4l2_ubuffers[buf.index].length,
+                 v4l2_ubuffers2[buf.index].start,
+                 v4l2_ubuffers2[buf.index].length);
+
+      buf2.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+      buf2.memory = V4L2_MEMORY_MMAP;
+      if (-1 == ioctl(fd2, VIDIOC_QBUF, &buf2)) {
+        fprintf(stderr, "VIDIOC_QBUF 2 failure\n");
+        return NULL;
+      }
+    }
+
+  }
+  return NULL;
+}
+
+int main(int argc, char const *argv[]) {
+
+  //Thisi for the first device
+  const char *device = "/dev/video0";
+  if (argc == 2 && (strchr(argv[1], 'h') != NULL)) {
+    print_help();
+    exit(0);
+  }
+
+  if (argc > 2) {
+    IMAGE_WIDTH = atoi(argv[1]);
+    IMAGE_HEIGHT = atoi(argv[2]);
+  }
+
+  if (argc > 3) {
+    device = argv[3];
+  }
+
+  int video_fildes = v4l2_open(device);
+  if (video_fildes == -1) {
+    fprintf(stderr, "can't open %s\n", device);
+    exit(-1);
+  }
+
+  if (v4l2_querycap(video_fildes, device) == -1) {
+    perror("v4l2_querycap");
+    goto exit_;
+  }
+
+  // most of devices support YUYV422 packed.
+  // V4L2_PIX_FMT_MJPEG
+  // V4L2_PIX_FMT_YUYV
+
+#if USE_MJPEG
+    int format= V4L2_PIX_FMT_MJPEG;
+#endif
+
+#if !USE_MJPEG
+    int format= V4L2_PIX_FMT_YUYV;
+#endif
+
+  if (v4l2_sfmt(video_fildes, format) == -1) {
+    perror("v4l2_sfmt");
+    goto exit_;
+  }
+
+  if (v4l2_gfmt(video_fildes) == -1) {
+    perror("v4l2_gfmt");
+    goto exit_;
+  }
+
+  if (v4l2_sfps(video_fildes, 5) == -1) { // no fatal error
+    perror("v4l2_sfps");
+  }
+
+  if (v4l2_mmap(video_fildes) == -1) {
+    perror("v4l2_mmap");
+    goto exit_;
+  }
+
+  if (v4l2_streamon(video_fildes) == -1) {
+    perror("v4l2_streamon");
+    goto exit_;
+  }
+
+  //end first device setup
+  //Thisi for the second device
+  const char *device2 = "/dev/video1";
+  int video_fildes2 = v4l2_open(device2);
+  if (video_fildes2 == -1) {
+    fprintf(stderr, "can't open %s\n", device2);
+    exit(-1);
+  }
+
+  if (v4l2_querycap(video_fildes2, device2) == -1) {
+    perror("v4l2_querycap");
+    goto exit_;
+  }
+
+  //// most of devices support YUYV422 packed.
+  if (v4l2_sfmt(video_fildes2, V4L2_PIX_FMT_YUYV) == -1) {
+    perror("v4l2_sfmt");
+    goto exit_;
+  }
+
+  if (v4l2_gfmt(video_fildes2) == -1) {
+    perror("v4l2_gfmt");
+    goto exit_;
+  }
+
+  if (v4l2_sfps(video_fildes2, 5) == -1) { // no fatal error
+    perror("v4l2_sfps");
+  }
+
+  if (v4l2_mmap2(video_fildes2) == -1) {
+      perror("v4l2_mmap");
+      goto exit_;
+  }
+
+
+  if (v4l2_streamon(video_fildes2) == -1) {
+    perror("v4l2_streamon");
+    goto exit_;
+  }
+  //end second device setup
+
+
+  // create a thread that will update frame int the buffer
+  struct streamHandler sH = {video_fildes, video_fildes2,frame_handler};
+  if (pthread_create(&thread_stream, NULL, v4l2_streaming, (void *)(&sH))) {
+    fprintf(stderr, "create thread failed\n");
+    goto exit_;
+  }
+
+  int quit = 0;
+  SDL_Event e;
+  while (!quit) {
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT) { // click close icon then quit
+        quit = 1;
+      }
+      if (e.type == SDL_KEYDOWN) {
+        if (e.key.keysym.sym == SDLK_ESCAPE) // press ESC the quit
+          quit = 1;
+      }
+    }
+    usleep(25);
+  }
+
+  thread_exit_sig = 1;               // exit thread_stream
+  pthread_join(thread_stream, NULL); // wait for thread_stream exiting
+
+  if (v4l2_streamoff(video_fildes) == -1) {
+    perror("v4l2_streamoff");
+    goto exit_;
+  }
+
+if (v4l2_streamoff(video_fildes2) == -1) {
+  perror("v4l2_streamoff 2");
+  goto exit_;
+}
+
+  if (v4l2_munmap() == -1) {
+    perror("v4l2_munmap");
+    goto exit_;
+  }
+if (v4l2_munmap2() == -1) {
+  perror("v4l2_munmap");
+  goto exit_;
+}
+
+exit_:
+  if (v4l2_close(video_fildes) == -1) {
+    perror("v4l2_close");
+  };
+  SDL_Quit();
+  return 0;
+}
---
+++ 3d2/v4l2_driver.c
@@ -0,0 +1,406 @@
+#include "v4l2_driver.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <linux/videodev2.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <math.h>
+
+
+int IMAGE_WIDTH = 640;
+int IMAGE_HEIGHT = 480;
+struct v4l2_ubuffer *v4l2_ubuffers;
+struct v4l2_ubuffer *v4l2_ubuffers2;
+
+
+unsigned char   *data_buffer1;
+unsigned char   *data_buffer2;
+unsigned char   *final_image;
+size_t stride;  
+
+
+int v4l2_open(const char *device) {
+  struct stat st;
+  memset(&st, 0, sizeof(st));
+  if (stat(device, &st) == -1) {
+    perror("stat");
+    return -1;
+  }
+  if (!S_ISCHR(st.st_mode)) {
+    fprintf(stderr, "%s is no character device\n", device);
+    return -1;
+  } else
+    printf("%s is a character device\n", device);
+  return open(device, O_RDWR | O_NONBLOCK, 0);
+}
+
+int v4l2_close(int fd) { return close(fd); }
+
+int v4l2_querycap(int fd, const char *device) {
+  struct v4l2_capability cap;
+  if (ioctl(fd, VIDIOC_QUERYCAP, &cap) == -1) {
+    printf("Error opening device %s: unable to query device.\n", device);
+    return -1;
+  } else {
+    printf("driver:\t\t%s\n", cap.driver);
+    printf("card:\t\t%s\n", cap.card);
+    printf("bus_info:\t%s\n", cap.bus_info);
+    printf("version:\t%d\n", cap.version);
+    printf("capabilities:\t%x\n", cap.capabilities);
+
+    if ((cap.capabilities & V4L2_CAP_VIDEO_CAPTURE) == V4L2_CAP_VIDEO_CAPTURE) {
+      printf("Device %s: supports capture.\n", device);
+    }
+
+    if ((cap.capabilities & V4L2_CAP_STREAMING) == V4L2_CAP_STREAMING) {
+      printf("Device %s: supports streaming.\n", device);
+    }
+  }
+
+  // emu all support fmt
+  struct v4l2_fmtdesc fmtdesc;
+  fmtdesc.index = 0;
+  fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  printf("\033[31mSupport format:\n\033[0m");
+  while (ioctl(fd, VIDIOC_ENUM_FMT, &fmtdesc) != -1) {
+    printf("\033[31m\t%d.%s\n\033[0m", fmtdesc.index + 1, fmtdesc.description);
+    fmtdesc.index++;
+  }
+  return 0;
+}
+
+// set format
+int v4l2_sfmt(int fd, uint32_t pfmt) {
+  // set fmt
+  struct v4l2_format fmt;
+  fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  fmt.fmt.pix.pixelformat = pfmt;
+  fmt.fmt.pix.height = IMAGE_HEIGHT;
+  fmt.fmt.pix.width = IMAGE_WIDTH;
+  fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
+
+  if (ioctl(fd, VIDIOC_S_FMT, &fmt) == -1) {
+    fprintf(stderr, "Unable to set format\n");
+    return -1;
+  }
+  return 0;
+}
+
+// get format
+int v4l2_gfmt(int fd) {
+  // set fmt
+  struct v4l2_format fmt;
+  if (ioctl(fd, VIDIOC_G_FMT, &fmt) == -1) {
+    fprintf(stderr, "Unable to get format\n");
+    return -1;
+  }
+
+  //set the stride
+ stride = fmt.fmt.pix.bytesperline;
+
+  printf("\033[33mpix.pixelformat:\t%c%c%c%c\n\033[0m",
+         fmt.fmt.pix.pixelformat & 0xFF, (fmt.fmt.pix.pixelformat >> 8) & 0xFF,
+         (fmt.fmt.pix.pixelformat >> 16) & 0xFF,
+         (fmt.fmt.pix.pixelformat >> 24) & 0xFF);
+  printf("pix.height:\t\t%d\n", fmt.fmt.pix.height);
+  printf("pix.width:\t\t%d\n", fmt.fmt.pix.width);
+  printf("pix.field:\t\t%d\n", fmt.fmt.pix.field);
+  return 0;
+}
+
+int v4l2_sfps(int fd, int fps) {
+  struct v4l2_streamparm setfps;
+  memset(&setfps, 0, sizeof(setfps));
+  setfps.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  setfps.parm.capture.timeperframe.numerator = 1;
+  setfps.parm.capture.timeperframe.denominator = fps;
+  if (ioctl(fd, VIDIOC_S_PARM, &setfps) == -1) {
+    // no fatal error ,just put err msg
+    fprintf(stderr, "Unable to set framerate\n");
+    return -1;
+  }
+  return 0;
+}
+
+int v4l2_mmap2(int fd) {
+
+  //map for rgb data
+  data_buffer2 = (unsigned char *) malloc(IMAGE_HEIGHT * IMAGE_WIDTH * 3 * sizeof(char));
+
+
+  // request for 4 buffers
+  struct v4l2_requestbuffers req;
+  req.count = BUF_NUM;
+  req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  req.memory = V4L2_MEMORY_MMAP;
+  if (ioctl(fd, VIDIOC_REQBUFS, &req) == -1) {
+    fprintf(stderr, "request for buffers error\n");
+    return -1;
+  }
+
+   // mmap for buffers
+  v4l2_ubuffers2 = malloc(req.count * sizeof(struct v4l2_ubuffer));
+  if (!v4l2_ubuffers2) {
+    fprintf(stderr, "Out of memory\n");
+    return -1;
+  }
+
+
+  struct v4l2_buffer buf;
+  unsigned int n_buffers;
+  for (n_buffers = 0; n_buffers < req.count; n_buffers++) {
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = n_buffers;
+    // query buffers
+    if (ioctl(fd, VIDIOC_QUERYBUF, &buf) == -1) {
+      fprintf(stderr, "query buffer error\n");
+      return -1;
+    }
+
+    v4l2_ubuffers2[n_buffers].length = buf.length;
+    // map 4 buffers in driver space to usersapce
+    v4l2_ubuffers2[n_buffers].start = mmap(
+        NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+#ifdef DEBUG
+    printf("buffer offset:%d\tlength:%d\n", buf.m.offset, buf.length);
+#endif
+    /**
+      *  output:
+      *  buffer offset:0	length:614400
+      *  buffer offset:614400	length:614400
+      *  buffer offset:1228800	length:614400
+      *  buffer offset:1843200	length:614400
+      *
+      *  explanation：saved in YUV422 format，a pixel needs 2 byte storage in
+      *  average，as our image size is 640*480. 640*480*2=614400
+    */
+    if (v4l2_ubuffers2[n_buffers].start == MAP_FAILED) {
+      fprintf(stderr, "buffer map error %u\n", n_buffers);
+      return -1;
+    }
+  }
+  return 0;
+}
+
+int v4l2_mmap(int fd) {
+
+  data_buffer1 = (unsigned char *) malloc(IMAGE_HEIGHT * IMAGE_WIDTH * 3 * sizeof(char));
+  final_image = (unsigned char *) malloc(IMAGE_HEIGHT * IMAGE_WIDTH * 3 * sizeof(char));
+
+  // request for 4 buffers
+  struct v4l2_requestbuffers req;
+  req.count = BUF_NUM;
+  req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  req.memory = V4L2_MEMORY_MMAP;
+  if (ioctl(fd, VIDIOC_REQBUFS, &req) == -1) {
+    fprintf(stderr, "request for buffers error\n");
+    return -1;
+  }
+
+  // mmap for buffers
+  v4l2_ubuffers = malloc(req.count * sizeof(struct v4l2_ubuffer));
+  if (!v4l2_ubuffers) {
+    fprintf(stderr, "Out of memory\n");
+    return -1;
+  }
+
+  struct v4l2_buffer buf;
+  unsigned int n_buffers;
+  for (n_buffers = 0; n_buffers < req.count; n_buffers++) {
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = n_buffers;
+    // query buffers
+    if (ioctl(fd, VIDIOC_QUERYBUF, &buf) == -1) {
+      fprintf(stderr, "query buffer error\n");
+      return -1;
+    }
+
+    v4l2_ubuffers[n_buffers].length = buf.length;
+    // map 4 buffers in driver space to usersapce
+    v4l2_ubuffers[n_buffers].start = mmap(
+        NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+#ifdef DEBUG
+    printf("buffer offset:%d\tlength:%d\n", buf.m.offset, buf.length);
+#endif
+    /**
+      *  output:
+      *  buffer offset:0	length:614400
+      *  buffer offset:614400	length:614400
+      *  buffer offset:1228800	length:614400
+      *  buffer offset:1843200	length:614400
+      *
+      *  explanation：saved in YUV422 format，a pixel needs 2 byte storage in
+      *  average，as our image size is 640*480. 640*480*2=614400
+    */
+    if (v4l2_ubuffers[n_buffers].start == MAP_FAILED) {
+      fprintf(stderr, "buffer map error %u\n", n_buffers);
+      return -1;
+    }
+  }
+  return 0;
+}
+
+int v4l2_munmap2() {
+  int i;
+  for (i = 0; i < BUF_NUM; i++) {
+    if (munmap(v4l2_ubuffers2[i].start, v4l2_ubuffers2[i].length) == -1) {
+      fprintf(stderr, "munmap 2 failure %d\n", i);
+      return -1;
+    }
+  }
+  return 0;
+}
+
+int v4l2_munmap() {
+  int i;
+  for (i = 0; i < BUF_NUM; i++) {
+    if (munmap(v4l2_ubuffers[i].start, v4l2_ubuffers[i].length) == -1) {
+      fprintf(stderr, "munmap failure %d\n", i);
+      return -1;
+    }
+  }
+  return 0;
+}
+
+int v4l2_streamon(int fd) {
+  // queue in the four buffers allocated by VIDIOC_REQBUFS, pretty like water
+  // filling a bottle in turn
+  struct v4l2_buffer buf;
+  buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  buf.memory = V4L2_MEMORY_MMAP;
+  unsigned int n_buffers;
+  for (n_buffers = 0; n_buffers < BUF_NUM; n_buffers++) {
+    buf.index = n_buffers;
+    if (ioctl(fd, VIDIOC_QBUF, &buf) == -1) {
+      fprintf(stderr, "queue buffer failed\n");
+      return -1;
+    }
+  }
+
+  enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  if (ioctl(fd, VIDIOC_STREAMON, &type) == -1) {
+    fprintf(stderr, "stream on failed\n");
+    return -1;
+  }
+  return 0;
+}
+
+int v4l2_streamoff(int fd) {
+  enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  if (-1 == ioctl(fd, VIDIOC_STREAMOFF, &type)) {
+    fprintf(stderr, "stream off failed\n");
+    return -1;
+  }
+  printf("stream is off\n");
+  return 0;
+}
+
+void compare_images(){
+    int x,y,yy;
+//we know the pixel will be to the right of the image
+
+// fprintf(stderr,"%d",image_1[20]);
+// fprintf(stderr,"\n");
+
+//  int j;
+//  for (j = 0; j + 1 < (IMAGE_WIDTH*IMAGE_HEIGHT); j += 1) {
+//          fprintf(stderr,"%d",image_1[j]);
+//  }
+
+  for (x = 0; x < (20); x += 1) {
+      for (y = 0; y < (IMAGE_WIDTH * 3); y += 3) {
+
+	  double d_smallest=1000;
+	  int best_match_pixel=0;
+          for (yy = y; yy > 3; yy -= 3) {
+                double d = sqrt(
+                      //pixel 1
+                  pow(data_buffer1[3*(x*IMAGE_WIDTH)+y]-data_buffer2[3*(x*IMAGE_WIDTH)+yy],2)+
+                  pow(data_buffer1[3*(x*IMAGE_WIDTH)+y+1]-data_buffer2[3*(x*IMAGE_WIDTH)+yy+1],2)+
+                  pow(data_buffer1[3*(x*IMAGE_WIDTH)+y+2]-data_buffer2[3*(x*IMAGE_WIDTH)+yy+2],2)+
+                      //pixel2
+                  pow(data_buffer1[x*IMAGE_WIDTH+y+1]-data_buffer2[x*IMAGE_WIDTH+yy+1],2)+
+                  pow(data_buffer1[x*IMAGE_WIDTH+y+1+1]-data_buffer2[x*IMAGE_WIDTH+yy+1+1],2)+
+                  pow(data_buffer1[x*IMAGE_WIDTH+y+2+1]-data_buffer2[x*IMAGE_WIDTH+yy+2+1],2)
+        /////////////         //pixel3
+/////////////   pow(data_buffer1[x*IMAGE_WIDTH+y+2]-data_buffer2[x*IMAGE_WIDTH+yy+2],2)+
+/////////////   pow(data_buffer1[x*IMAGE_WIDTH+y+1+2]-data_buffer2[x*IMAGE_WIDTH+yy+1+2],2)+
+/////////////   pow(data_buffer1[x*IMAGE_WIDTH+y+2+2]-data_buffer2[x*IMAGE_WIDTH+yy+2+2],2)
+
+                 );
+
+             //  fprintf(stderr,"%d \n",(int) d);
+                 if(d < d_smallest){
+                    d_smallest=d;
+                 }
+                 if(d_smallest <=20){
+                     best_match_pixel=yy;
+                     break;
+                 }
+
+          }
+          //if we found a match
+          if(best_match_pixel){
+              int distance=best_match_pixel-y;
+              int color=255 - round(distance * .6375);
+              color=100;
+               final_image[3*(x*IMAGE_WIDTH)+y]=color;
+               final_image[3*(x*IMAGE_WIDTH)+y+1]=color;
+               final_image[3*(x*IMAGE_WIDTH)+y+2]=color;
+          }else{
+               final_image[3*(x*IMAGE_WIDTH)+y]=0;
+               final_image[3*(x*IMAGE_WIDTH)+y+1]=0;
+               final_image[3*(x*IMAGE_WIDTH)+y+2]=0;
+          }
+      }
+  }
+
+         fprintf(stderr,"Cool we are done \n");
+}
+
+/*****
+ * Taken from libv4l2 (in v4l-utils)
+ *
+ * (C) 2008 Hans de Goede <hdegoede@redhat.com>
+ *
+ * Released under LGPL
+ */
+#define CLIP(color) (unsigned char)(((color) > 0xFF) ? 0xff : (((color) < 0) ? 0 : (color)))
+
+void v4lconvert_yuyv_to_rgb24(const unsigned char *src, unsigned char *dest ) {
+    int j;
+    int width=IMAGE_WIDTH;
+    int height=IMAGE_HEIGHT;
+
+    while (--height >= 0) {
+        for (j = 0; j + 1 < width; j += 2) {
+            int u = src[1];
+            int v = src[3];
+            int u1 = (((u - 128) << 7) +  (u - 128)) >> 6;
+            int rg = (((u - 128) << 1) +  (u - 128) +
+                    ((v - 128) << 2) + ((v - 128) << 1)) >> 3;
+            int v1 = (((v - 128) << 1) +  (v - 128)) >> 1;
+
+            *dest++ = CLIP(src[0] + v1);
+            *dest++ = CLIP(src[0] - rg);
+            *dest++ = CLIP(src[0] + u1);
+
+            *dest++ = CLIP(src[2] + v1);
+            *dest++ = CLIP(src[2] - rg);
+            *dest++ = CLIP(src[2] + u1);
+            src += 4;
+        }
+        src += stride - (width * 2);
+    }
+}
+
