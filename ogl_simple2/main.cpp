460a4859fb8560e62a4c201ecf340fe66927de6a
TaylorHawkes <th71852@gmail.com>
2018-03-26 16:48:51 -0500

    fun with open gl

--- ogl_simple2/main.cpp
+++ ogl_simple2/main.cpp
@@ -31,15 +31,53 @@
 //#include <glm/gtx/quaternion.hpp>
 
 
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
 
-typedef struct {
+
+
+
+struct Vec3{
     float x,y,z;
-} Vec3;
+ 	Vec3(float x = 0,float y=0,float z=0): x(x), y(y),z(z){} 
+};
 
 
-typedef struct {
+struct Mat4{
     float m[16];
-} Mat4;
+
+	 Mat4 operator *(Mat4 b) {
+		Mat4 r;
+		r.m[0]  = (this->m[0]*b.m[0]) + (this->m[1]*b.m[4]) +(this->m[2]*b.m[8]) + (this->m[3]*b.m[12]);
+		r.m[1]  = (this->m[0]*b.m[1]) + (this->m[1]*b.m[5]) +(this->m[2]*b.m[9]) + (this->m[3]*b.m[13]);
+		r.m[2]  = (this->m[0]*b.m[2]) + (this->m[1]*b.m[6]) +(this->m[2]*b.m[10])+ (this->m[3]*b.m[14]);
+		r.m[3]  = (this->m[0]*b.m[3]) + (this->m[1]*b.m[7]) +(this->m[2]*b.m[11])+ (this->m[3]*b.m[15]);
+
+		r.m[4]  = (this->m[4]*b.m[0]) + (this->m[5]*b.m[4]) +(this->m[6]*b.m[8]) + (this->m[7]*b.m[12]);
+		r.m[5]  = (this->m[4]*b.m[1]) + (this->m[5]*b.m[5]) +(this->m[6]*b.m[9]) + (this->m[7]*b.m[13]);
+		r.m[6]  = (this->m[4]*b.m[2]) + (this->m[5]*b.m[6]) +(this->m[6]*b.m[10])+ (this->m[7]*b.m[14]);
+		r.m[7]  = (this->m[4]*b.m[3]) + (this->m[5]*b.m[7]) +(this->m[6]*b.m[11])+ (this->m[7]*b.m[15]);
+
+		r.m[8]  = (this->m[8]*b.m[0]) + (this->m[9]*b.m[4]) +(this->m[10]*b.m[8])+  (this->m[11]*b.m[12]);
+		r.m[9]  = (this->m[8]*b.m[1]) + (this->m[9]*b.m[5]) +(this->m[10]*b.m[9])+  (this->m[11]*b.m[13]);
+		r.m[10] = (this->m[8]*b.m[2]) + (this->m[9]*b.m[6]) +(this->m[10]*b.m[10])+ (this->m[11]*b.m[14]);
+		r.m[11] = (this->m[8]*b.m[3]) + (this->m[9]*b.m[7]) + (this->m[10]*b.m[11]) + (this->m[11]*b.m[15]);
+
+		r.m[12] = (this->m[12]*b.m[0]) + (this->m[13]*b.m[4]) + (this->m[14]*b.m[8]) + (this->m[15]*b.m[12]);
+		r.m[13] = (this->m[12]*b.m[1]) + (this->m[13]*b.m[5]) + (this->m[14]*b.m[9]) + (this->m[15]*b.m[13]);
+		r.m[14] = (this->m[12]*b.m[2]) + (this->m[13]*b.m[6]) + (this->m[14]*b.m[10])+ (this->m[15]*b.m[14]);
+		r.m[15] = (this->m[12]*b.m[3]) + (this->m[13]*b.m[7]) + (this->m[14]*b.m[11])+ (this->m[15]*b.m[15]);
+
+		return r;
+
+      }	
+	
+
+
+};
+
 
 typedef struct {                                                                              
     float x=0;
@@ -49,6 +87,7 @@ typedef struct {
 } Quat;
 
 void updateQuatByRotation(Quat *q, Vec3 *v);
+
 void quat_mul(Quat *q1,Quat *q2,Quat *r);
 void quatToMatrix(Quat q_bad,Mat4 *m);
 void toEulerAngle(Quat& q, double &roll, double &pitch, double &yaw);
@@ -59,10 +98,17 @@ Vec3 v_cross( Vec3 a, Vec3 b);
 Vec3 v_mult_s( Vec3 v,float m);
 Vec3 rotate_vector_by_quaternion(Vec3 v, Quat q);
 void mat_mult_v(Mat4 m,Vec3 v1,Vec3 *r);
-
+void v(Mat4 m,Vec3 v1,Vec3 *r);
+Vec3 v_norm(Vec3 v);
 void quat_norm(Quat *q);
 
-
+Vec3 v_sub( Vec3 a, Vec3 b){
+        Vec3 v;
+        v.x=a.x-b.x;
+        v.y=a.y-b.y;
+        v.z=a.z-b.z;
+        return v;
+}
 
 void updateQuatByRotation(Quat *q, Vec3 *v){
 
@@ -79,6 +125,7 @@ void updateQuatByRotation(Quat *q, Vec3 *v){
         //maybye
         quat_norm(q);
 }
+
 
 void quat_norm(Quat *q){
  float d= (q->w*q->w) + (q->x*q->x) + (q->y*q->y) + (q->z*q->z);
@@ -111,9 +158,41 @@ void quat_mul(Quat *q1,Quat *q2,Quat *r){
     r->z = (q1->w*q2->z + q1->x*q2->y - q1->y*q2->x + q1->z*q2->w);
 }
 
+void updateTranslationMatrix(Mat4 *m, Vec3 v){
+    m->m[12]+=v.x;
+    m->m[13]+=v.y;
+    m->m[14]+=v.z;
+}
+
+Mat4 glm_to_mat(glm::mat4 gm){
+
+    Mat4 m;
+    m.m[0] =(float) gm[0].x;
+    m.m[1] =(float) gm[0].y; 
+    m.m[2] =(float) gm[0].z; 
+    m.m[3] =(float) gm[0].w; 
+
+    m.m[4] =(float) gm[1].x;
+    m.m[5] =(float) gm[1].y; 
+    m.m[6] =(float) gm[1].z; 
+    m.m[7] =(float) gm[1].w; 
+
+    m.m[8] =(float)  gm[2].x;  
+    m.m[9] =(float)  gm[2].y; 
+    m.m[10] =(float) gm[2].z; 
+    m.m[11] =(float) gm[2].w; 
+
+    m.m[12] =(float) gm[3].x;  
+    m.m[13] =(float) gm[3].y; 
+    m.m[14] =(float) gm[3].z; 
+    m.m[15] =(float) gm[3].w; 
+	return m;
+}
+
+
 
 //I still dont understand this...
-void getProjectionMatrix( Mat4 *m,float angleOfView,float imageAspectRatio, float zMin, float zMax)
+void getProjectionMatrix(Mat4 *m,float angleOfView,float imageAspectRatio, float zMin, float zMax)
 {
 	float ang = tan((angleOfView*.5)*M_PI/180);//angle*.5                                                    
     m->m[0] = 0.5/ang; 
@@ -159,13 +238,13 @@ void getViewMatrix(Mat4 *m){
 	//This is like camera position mmmk
 	m->m[12]=0; //x
     m->m[13]=5;//y
-    m->m[14]=20;//z
+    m->m[14]=10;//z
     m->m[15]=1;
 	
    //and we flip
-   m->m[12]*=-1;
-   m->m[13]*=-1;
-   m->m[14]*=-1;
+  //m->m[12]*=-1;
+  //m->m[13]*=-1;
+  //m->m[14]*=-1;
 
 }
 
@@ -194,27 +273,28 @@ void getIdentityMatrix(Mat4 *r){
     r->m[15] =1;
 }
 
-void matrix_product(Mat4 *a,Mat4 *b, Mat4 *r){
-    r->m[0]  = (a->m[0]*b->m[0]) + (a->m[1]*b->m[4]) +(a->m[2]*b->m[8]) + (+a->m[3]*b->m[12]);
-    r->m[1]  = (a->m[0]*b->m[1]) + (a->m[1]*b->m[5]) +(a->m[2]*b->m[9]) +(+a->m[3]*b->m[13]);
-    r->m[2]  = (a->m[0]*b->m[2]) + (a->m[1]*b->m[6]) +(a->m[2]*b->m[10])+ (+a->m[3]*b->m[14]);
-    r->m[3]  = (a->m[0]*b->m[3]) + (a->m[1]*b->m[7]) +(a->m[2]*b->m[11])+ (+a->m[3]*b->m[15]);
+void getTranslationMatrix(Mat4 *r,float x, float y, float z){
+    r->m[0] =1;
+    r->m[1] =0;
+    r->m[2] =0;
+    r->m[3] =0;
 
-    r->m[4]  = (a->m[4]*b->m[0]) + (a->m[5]*b->m[4]) +(a->m[6]*b->m[8])+ (+a->m[7]*b->m[12]);
-    r->m[5]  = (a->m[4]*b->m[1]) + (a->m[5]*b->m[5]) +(a->m[6]*b->m[9])+ (+a->m[7]*b->m[13]);
-    r->m[6]  = (a->m[4]*b->m[2]) + (a->m[5]*b->m[6]) +(a->m[6]*b->m[10])+ (+a->m[7]*b->m[14]);
-    r->m[7]  = (a->m[4]*b->m[3]) + (a->m[5]*b->m[7]) +(a->m[6]*b->m[11])+(+a->m[7]*b->m[15]);
+    r->m[4] =0;
+    r->m[5] =1;
+    r->m[6] =0;
+    r->m[7] =0;
 
-    r->m[8]  = (a->m[8]*b->m[0]) + (a->m[9]*b->m[4]) +(a->m[10]*b->m[8])+ (+a->m[11]*b->m[12]);
-    r->m[9]  = (a->m[8]*b->m[1]) + (a->m[9]*b->m[5]) +(a->m[10]*b->m[9])+ (+a->m[11]*b->m[13]);
-    r->m[10] = (a->m[8]*b->m[2]) + (a->m[9]*b->m[6]) +(a->m[10]*b->m[10])+ (+a->m[11]*b->m[14]);
-    r->m[11] = (a->m[8]*b->m[3]) + (a->m[9]*b->m[7]) +(a->m[10]*b->m[11])+(+a->m[11]*b->m[15]);
+    r->m[8] =0;
+    r->m[9] =0;
+    r->m[10] =1;
+    r->m[11] =0;
 
-    r->m[12] = (a->m[12]*b->m[0]) + (a->m[13]*b->m[4]) +(a->m[14]*b->m[8]) + (+a->m[15]*b->m[12]);
-    r->m[13] = (a->m[12]*b->m[1]) + (a->m[13]*b->m[5]) +(a->m[14]*b->m[9]) +(+a->m[15]*b->m[13]);
-    r->m[14] = (a->m[12]*b->m[2]) + (a->m[13]*b->m[6]) +(a->m[14]*b->m[10])+ (+a->m[15]*b->m[14]);
-    r->m[15] = (a->m[12]*b->m[3]) + (a->m[13]*b->m[7]) +(a->m[14]*b->m[11])+ (+a->m[15]*b->m[15]);
+    r->m[12] =x;//x,y,z
+    r->m[13] =y;
+    r->m[14] =z;
+    r->m[15] =1;
 }
+
 
 void print_matrix(Mat4 *a){
 		fprintf(stderr, "%f %f %f %f \n",a->m[0],a->m[1],a->m[2],a->m[3]);
@@ -226,6 +306,37 @@ void print_matrix(Mat4 *a){
 void print_quat(Quat *q){
 		fprintf(stderr, "%f %f %f %f \n",q->w,q->x,q->y,q->z);
 }
+
+
+Mat4 lookAt(Vec3 Eye, Vec3 Center,Vec3 Up){
+	Vec3 f = v_norm(v_sub(Center, Eye));
+	Vec3 s = v_norm(v_cross(f,Up));
+	Vec3 u = v_cross(s,f);
+
+        Mat4 m;
+		m.m[0] = s.x;
+		m.m[1] = s.y;
+		m.m[2] = s.z;
+		m.m[3] = 0;
+
+		m.m[4] = u.x;
+		m.m[5] = u.y;
+		m.m[6] = u.z;
+		m.m[7] = 0;
+
+		m.m[8] =-f.x;
+		m.m[9] =-f.y;
+		m.m[10]=-f.z;
+		m.m[11]=0;
+
+		m.m[12]=-v_dot(s, Eye);
+		m.m[13]=-v_dot(u, Eye);
+		m.m[14]= v_dot(f, Eye);
+		m.m[15]=1;
+
+		return  m;
+}
+
 
 
 //HHEADER declrations
@@ -241,6 +352,7 @@ Mat4 View;
 Mat4 Model;
 Mat4 Rotation;
 Mat4 Projection;
+Mat4 Translation;
 Mat4 MVP;
 Vec3 gyro;
 Quat ooo;
@@ -252,8 +364,11 @@ double yaw;
 
 int video_fildes,video_fildes2;
 float a_pitch_final,a_roll_final;
-int16_t ax, ay, az;
+int16_t ax_bad, ay_bad, az_bad;
 int16_t gx, gy, gz;
+
+float ax,ay,az;
+
 //glm::vec3 velocity;
 int dt; //in microseconds
 float delta_seconds;
@@ -273,8 +388,6 @@ float verticalAngle = 0.0f;
 float initialFoV = 45.0f;
 float speed = 3.0f; // 3 units / second
 float mouseSpeed = 0.005f;
-
-
 
 
 
@@ -360,20 +473,20 @@ float mouseSpeed = 0.005f;
 
 
 
-
+float avg=0;
+float avg_count=0;
 int main( void )
 {
 
-
-
     int zoom =1;
     //currentOrientation=glm::quat(glm::vec3(0,0,0));
-    getProjectionMatrix(&Projection, 40+(zoom*2), 1, -100, -1);
+    getProjectionMatrix(&Projection, 40+(zoom*2), 1, .01, 100);
     getIdentityMatrix(&Model);        
-    getViewMatrix(&View);     
+    getTranslationMatrix(&Translation,0,0,0);        
+	View=lookAt(Vec3(0,4,-20),Vec3(0,0,0),Vec3(0,1,0));
+
 
     std::thread t1(updateGyroPositionThead);//crank of the new thread
-    //start_cameras();
 
     glfwInit() ;//no eror
 	glfwWindowHint(GLFW_SAMPLES, 4);
@@ -390,8 +503,17 @@ int main( void )
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	// Enable depth test
+//	glEnable(GL_DEPTH_TEST);
+	// Accept fragment if it closer to the camera than the former one
+//	glDepthFunc(GL_LESS); 
+
+
+
+
+
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
@@ -405,6 +527,8 @@ int main( void )
     GLuint vertexbuffer;
     glGenBuffers(1, &vertexbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+
+    //-z is out of screen, +z is going into screen (ie:away from you taylor)
     GLfloat g_vertex_buffer_data[] = { 
         //rect front
     -4.0f,0.5f,2.0f, 
@@ -450,9 +574,9 @@ int main( void )
     -4.0f, 0.5f,-2.0f,
 
     //forward triange
-	 -2.0f,0.5f,-2.0f, 
-	  2.0f,0.5f,-2.0f, 
-	  0.0f,0.5f,-7.0f
+	 -2.0f,0.5f,2.0f, 
+	  2.0f,0.5f,2.0f, 
+	  0.0f,0.5f,7.0f
 	};
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
@@ -476,22 +600,39 @@ int main( void )
     glUseProgram(programID);
 	
     //computeMatricesFromInputs();
-	//Update Model by Rotation
-	//quatToMatrix(ooo,&Rotation);
 
-////Mat4 t;
-////getIdentityMatrix(&t);      
-//matrix_product(&Rotation,&t,&Model);
+	//Update Model by Rotation
+
+    ////Mat4 t;
+    ////getIdentityMatrix(&t);      
 
 	//print_matrix(&Rotation);
 
-    matrix_product(&Rotation,&View,&MVP);
-    matrix_product(&MVP,&Projection,&MVP);
+ // matrix_product(&Model,&Rotation,&MVP);
+ // matrix_product(&MVP,&Translation,&MVP);
+
+  //Mat4 MVP = matrix_product(matrix_product(Model,View),Projection);
+
+   Mat4 MVP = Model * Rotation * View * Projection ; //Taylor Does not do this backwords
+
+   // glm::mat4 MVP2 = Projection_glm * View_glm ; //This multiplies Model*View, then takes that output and multiplies by Projection 
+
+  //  Mat4 MVP=glm_to_mat(MVP2);
+	
+////Mat4 p2 glm_to_mat(Projection_glm);
+
+////print_matrix(&Projection);	
+////print_matrix(p2);	
+
+//return 1;
+
 
  // Send our transformation to the currently bound shader, 
  // in the "MVP" uniform
 
  glUniformMatrix4fv(MatrixID, 1, GL_FALSE, MVP.m);
+ //glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
  // 1rst attribute buffer : vertices
  glEnableVertexAttribArray(0);
  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -533,9 +674,37 @@ void updateGyroPositionThead(){
 
  fprintf(stderr, " I am my own thread \n");
  accelgyro.initialize();//init accel/gyro
+// accelgyro.reset();
+ //return;
+//accelgyro.setXAccelOffset(0);//init accel/gyro
+//accelgyro.setYAccelOffset(0);//init accel/gyro
+//int16_t t=20;
+//accelgyro.setZAccelOffset(t);//init accel/gyro
+
+
+///  int xOffset=accelgyro.getXAccelOffset();//init accel/gyro
+///  int yOffset=accelgyro.getYAccelOffset();//init accel/gyro
+//  int zOffset=accelgyro.getZAccelOffset();//init accel/gyro
+
+/// fprintf(stderr, "offset: %i\n", xOffset);
+/// fprintf(stderr, "offset: %i\n", yOffset);
+ // fprintf(stderr, "offset: %i\n", zOffset);
+ // return;
+
 	auto this_time = std::chrono::high_resolution_clock::now();
 	auto last_time = std::chrono::high_resolution_clock::now();
   int i=0;
+
+  float x_bias_gain=1.007;
+  float x_bias_offset=-254.26;
+
+  float y_bias_gain=1.00097;
+  float y_bias_offset=58.060;
+
+  float z_bias_gain=0.97873;
+  float z_bias_offset=-415.9617;
+
+  float z_count=0;
   do{
 
   usleep(10000);
@@ -544,7 +713,18 @@ void updateGyroPositionThead(){
   auto dt = std::chrono::duration_cast<std::chrono::microseconds>(this_time - last_time).count();
   last_time = std::chrono::high_resolution_clock::now();
 
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  accelgyro.getMotion6(&ax_bad, &ay_bad, &az_bad, &gx, &gy, &gz);
+
+  ax=((float) ax_bad * x_bias_gain) + x_bias_offset;
+  ay=((float) ay_bad * y_bias_gain) + y_bias_offset;
+  az=((float) az_bad * z_bias_gain) + z_bias_offset;
+
+  //az-=500;//rough calibration
+  //fix az bias fix
+  //goal is to get to 4096
+  //az-=300;
+
+  //printf("a/g: %f %f %f   %6hd %6hd %6hd\n",ax,ay,az,gx,gy,gz);
 
   delta_seconds= ((float) dt) / 1000000;
 
@@ -554,23 +734,32 @@ void updateGyroPositionThead(){
 // gyro.y=(-gz/GYRO_DPS) * DEGREES_TO_RADIANS * delta_seconds; //this moves  yaw (z apparently)
 // gyro.z= (-gx/GYRO_DPS) * DEGREES_TO_RADIANS * delta_seconds;//roll
 
- gyro.x=(-gx/GYRO_DPS) * DEGREES_TO_RADIANS * delta_seconds;//roll
- gyro.y=(-gy/GYRO_DPS) * DEGREES_TO_RADIANS * delta_seconds; //pitch
- gyro.z=(gz/GYRO_DPS) * DEGREES_TO_RADIANS * delta_seconds;//yaw
+   gyro.x=(-gx/GYRO_DPS) * DEGREES_TO_RADIANS * delta_seconds;//roll
+   gyro.y=(-gy/GYRO_DPS) * DEGREES_TO_RADIANS * delta_seconds; //pitch
+   gyro.z=(-gz/GYRO_DPS) * DEGREES_TO_RADIANS * delta_seconds;//yaw
 
-  updateQuatByRotation(&ooo,&gyro); 
-  toEulerAngle(ooo,roll,pitch,yaw);
+   updateQuatByRotation(&ooo,&gyro); 
+
+   toEulerAngle(ooo,roll,pitch,yaw);
 
 
 	float acc_total_vector = sqrt((ax*ax)+(ay*ay)+(az*az));
+
+
+    //this corrects wrong if we flip all the way over
 	if(abs(ax) < acc_total_vector){
 		 a_pitch_final = asin((float)ax/acc_total_vector);
 	}
+
 	if(abs(ay) < acc_total_vector){
 		 a_roll_final = -asin((float)ay/acc_total_vector);
 	}
 
+  //printf("Roll: %f \n",a_roll_final);
+
 ////double gyro_yaw,gyro_pitch,gyro_roll; 
+
+    //fprintf(stderr, " A: %f, C:%f  \n",roll,a_roll_final);
 
 	//fprintf(stderr, " pitch accel: %f \n",a_pitch_final);
 	//fprintf(stderr, " pitch: %f \n,  ",gyro_roll/0.0174);
@@ -579,25 +768,91 @@ void updateGyroPositionThead(){
 		
 	
 	Vec3 accell_correction;
-	accell_correction.x=(a_roll_final-roll) * 5 * delta_seconds;
-	accell_correction.y=(a_pitch_final-pitch) * 5 * delta_seconds ;
+	accell_correction.x=(a_roll_final - roll) * 5 * delta_seconds;
+	accell_correction.y=(a_pitch_final- pitch) * 5 * delta_seconds ;
 	accell_correction.z=0;//(a_roll_final-gyro_roll) * .02;
 
-    updateQuatByRotation(&ooo,&accell_correction); 
+    //updateQuatByRotation(&ooo,&accell_correction); 
 
 
 	//Vec3 ff_vec=rotate_vector_by_quaternion(f_vec,ooo);
 
-	quatToMatrix(ooo,&Rotation);
-    //forward vector:
-    Vec3 f_vec; 
-    f_vec.x = 0;
-    f_vec.y = 0;
-    f_vec.z = 1;
-    Vec3 ff_vec;
-    mat_mult_v(Rotation,f_vec,&ff_vec);
+////quatToMatrix(ooo,&Rotation);
 
-	fprintf(stderr, " pitch accel x,y,z: %f, %f, %f \n",ff_vec.x,ff_vec.y,ff_vec.z);
+//////downward vector:
+////Vec3 d_vec; 
+////d_vec.x = 0; d_vec.y = -1; d_vec.z = 0;
+////Vec3 dd_vec;
+////mat_mult_v(Rotation,d_vec,&dd_vec);
+
+//////forward vector:
+////Vec3 f_vec; 
+////f_vec.x = 0; f_vec.y = 0; f_vec.z = -1;
+////Vec3 ff_vec;
+////mat_mult_v(Rotation,f_vec,&ff_vec);
+
+//////right vector:
+////Vec3 r_vec; 
+////r_vec.x = 1; r_vec.y = 0; r_vec.z = 0;
+////Vec3 rr_vec;
+////mat_mult_v(Rotation,r_vec,&rr_vec);
+
+
+////float d_dot=v_dot(d_vec,dd_vec);
+
+
+////float f_dot=v_dot(f_vec,ff_vec);
+////float r_dot=v_dot(r_vec,rr_vec);
+
+
+////float expected_downward_accell=d_dot*4096;
+
+////float actual_downward_accell=d_dot*az;
+////float actual_forward_accell=f_dot*ax;
+////float actual_right_accell=r_dot*ay;
+
+//////fprintf(stderr, " Dot: %f,  \n",d_dot);
+
+//////fprintf(stderr, " Actual forard: %f,  \n",actual_forward_accell);
+//////fprintf(stderr, " A down: %f, E down: %f \n",actual_downward_accell,expected_downward_accell);
+////float relative_accell_downward=actual_downward_accell-expected_downward_accell;
+////    
+
+//// Vec3 accell_vector_down;
+//// accell_vector_down.x=1;
+//// accell_vector_down.y=0;//relative_accell_downward;
+//// accell_vector_down.z=0;
+
+
+////Vec3 accell_vector_relative;
+////mat_mult_v(Rotation,accell_vector_down,&accell_vector_relative);
+
+////fprintf(stderr, " x,y,z: %f,%f,%f \n",accell_vector_relative.x,accell_vector_relative.y,accell_vector_relative.z);
+
+    //updateTranslationMatrix(&Translation,accell_vector_relative);
+
+
+    //fprintf(stderr, " A down: %f, E down: %f \n",Translation.m[14]);
+    
+
+
+
+
+
+      
+
+ // avg=avg+sqrt(ax*ax+ay*ay+az*az);
+ // avg_count+=1;
+    
+    //fprintf(stderr, " Total: %f \n",(float) avg/avg_count);
+
+//    fprintf(stderr, " Total: %f \n", (float) abs(actual_downward_accell) + abs(actual_forward_accell) + abs(actual_right_accell));
+
+
+
+
+
+
 
 	
 
@@ -790,86 +1045,28 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
 }
 
 void quatToMatrix(Quat q_bad,Mat4 *m){
-    
-	//I want x to be roll
-	quat_norm(&q_bad);
 
-	Quat q;
-	q.x=q_bad.y;
-	q.y=q_bad.z;//
-	q.z=q_bad.x;
-	
-float x=q.x;
-float y=q.y;
-float z=q.z;
-float w=q.w;
+    float x=q_bad.y;
+    float y=q_bad.z; //y controls yaw
+    float z=q_bad.x;
+    float w=q_bad.w;
 
-////float wx, wy, wz, xx, yy, yz, xy, xz, zz, x2, y2, z2;
-////x2 = q.x + q.x;
-////y2 = q.y + q.y;
-////z2 = q.z + q.z;
-////xx = q.x * x2;
-////xy = q.x * y2;
-////xz = q.x * z2;
-////yy = q.y * y2;
-////yz = q.y * z2;
-////zz = q.z * z2;
-////wx = q.w * x2;
-////wy = q.w * y2;	
-////wz = q.w * z2;
-
-////R = [
-////1 - 2*y*y - 2*z*z,
-////2*x*y - 2*z*w, 
-////2*x*z + 2*y*w;
-
-////2*x*y + 2*z*w,
-////1 - 2*x*x - 2*z*z,
-////2*y*z - 2*x*w;
-
-////2*x*z - 2*y*w,
-////2*y*z + 2*x*w,
-////1 - 2*x*x - 2*y*y
-
-//// ];
-
-  m->m[0] =1 - 2*y*y - 2*z*z;
-  m->m[1] = 2*x*y - 2*z*w ; 
-  m->m[2] = 2*x*z + 2*y*w; 
-  m->m[3]= 0;
-  m->m[4] = 2*x*y + 2*z*w ;
-  m->m[5] = 1 - 2*x*x - 2*z*z; 
-  m->m[6] = 2*y*z - 2*x*w;    
-  m->m[7] = 0;
-  m->m[8] = 2*x*z - 2*y*w;  
-  m->m[9] = 2*y*z + 2*x*w;  
-  m->m[10]= 1 - 2*x*x - 2*y*y;
-  m->m[11] = 0;
-  m->m[12] =0;
-  m->m[13] =0;
-  m->m[14] =0;
-  m->m[15] =1;
-
-
-//  m->m[0] =1.0f-(yy + zz); 
-//  m->m[1] = xy + wz; 
-//  m->m[2] = xz - wy; 
-//  m->m[3]= 0;
-//          
-//  m->m[4] = xy - wz;
-//  m->m[5] = 1.0f-(xx + zz); 
-//  m->m[6] = yz + wx;   
-//  m->m[7] = 0;
-//          
-//  m->m[8] = xz + wy;  
-//  m->m[9] = yz - wx;  
-//  m->m[10]= 1.0f-(xx + yy);
-//  m->m[11] = 0;
-//          
-//  m->m[12] =0;
-//  m->m[13] =0;
-//  m->m[14] =0;
-//  m->m[15] =1;
+    m->m[0] =1 - 2*y*y - 2*z*z;
+    m->m[1] = 2*x*y - 2*z*w ; 
+    m->m[2] = 2*x*z + 2*y*w; 
+    m->m[3]= 0;
+    m->m[4] = 2*x*y + 2*z*w ;
+    m->m[5] = 1 - 2*x*x - 2*z*z; 
+    m->m[6] = 2*y*z - 2*x*w;    
+    m->m[7] = 0;
+    m->m[8] = 2*x*z - 2*y*w;  
+    m->m[9] = 2*y*z + 2*x*w;  
+    m->m[10]= 1 - 2*x*x - 2*y*y;
+    m->m[11] = 0;
+    m->m[12] =0;
+    m->m[13] =0;
+    m->m[14] =0;
+    m->m[15] =1;
 
 }
 
@@ -922,9 +1119,9 @@ Vec3 v_norm(Vec3 v){
 }
 
 float v_dot( Vec3 v1, Vec3 v2){  
-   Vec3 v1n=v_norm(v1); 
-   Vec3 v2n=v_norm(v2); 
-  return v1n.x * v2n.x + v1n.y * v2n.y + v1n.z * v2n.z; 
+   //Vec3 v1n=v_norm(v1); 
+   //Vec3 v2n=v_norm(v2); 
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; 
 }
 
 Vec3 v_cross( Vec3 a, Vec3 b){
